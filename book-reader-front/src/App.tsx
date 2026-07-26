f60e39041bc5e1a73d1b308bec758f2f2033c5bd
Oleg <oleg.a.sysa@gmail.com>
2026-07-26 18:23:23 +0300

    added front invoke for healthcheck

--- book-reader-front/src/App.tsx
+++ book-reader-front/src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [status, setStatus] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("https://localhost:7266/api/health") // замени на свой порт
+      .then((response) => response.json())
+      .then((data) => setStatus(data.status))
+      .catch((error) => {
+        console.error(error);
+        setStatus("Backend unavailable");
+      });
+  }, []);
+
   return (
-    <h1>BookReader</h1>
+    <div style={{ padding: "2rem" }}>
+      <h1>BookReader</h1>
+      <p>Backend status: {status}</p>
+    </div>
   );
 }
 
