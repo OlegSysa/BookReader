b9477126dd4f97cbc967d88df479f30f0bd6c407
Oleg <oleg.a.sysa@gmail.com>
2026-08-10 14:17:52 +0300

    added logs to parsing logic

--- book-reader-backend/BookReader.BookProcessor/Consumers/UploadBookConsumer.cs
+++ book-reader-backend/BookReader.BookProcessor/Consumers/UploadBookConsumer.cs
@@ -7,13 +7,13 @@ namespace BookReader.BookProcessor.Consumers
     public class UploadBookConsumer : IConsumer<BookUploadedEvent>
     {
         private readonly IBookParserService _bookParserService;
-
         public UploadBookConsumer(IBookParserService bookParserService)
         {
             _bookParserService = bookParserService;
         }
         public async Task Consume(ConsumeContext<BookUploadedEvent> context)
         {
+
             await _bookParserService.ParseBook(
                 context.Message.BookId,
                 context.CancellationToken);
--- book-reader-backend/BookReader.Core/Business/BookParserService.cs
+++ book-reader-backend/BookReader.Core/Business/BookParserService.cs
@@ -26,32 +26,45 @@ namespace BookReader.Core.Business
         }
         public async Task<bool> ParseBook(int bookId, CancellationToken token)
         {
-            var book = await _repository.GetByIdAsync(bookId, token);
-            if (book == null)
+            try
             {
-                _logger.LogError("Book with id '{BookId}' was not found.", bookId);
+                _logger.LogInformation("[BOOK PROCESSING] STARTED. BookId: {BookId}", bookId);
+                var book = await _repository.GetByIdAsync(bookId, token);
+                if (book == null)
+                {
+                    _logger.LogError("Book with id '{BookId}' was not found.", bookId);
+                    return false;
+                }
+                var parser = GetParser(book.OriginalFileName);
+                if (parser == null)
+                {
+                    _logger.LogError("[BOOK PROCESSING] Parser is NULL");
+                    return false;
+                }
+
+                book.Status = BookStatus.ParseProcessing;
+                await _repository.SaveChangesAsync();
+
+                var chapters = await parser.ParseFile(book.StoragePath);
+                _logger.LogInformation("[BOOK PROCESSING] PARSED. BookId: {BookId}", bookId);
+                var storageRootPath = _config["Storage:ParsedBooksPath"] ?? string.Empty;
+
+                var savingResult = await _storageService.SaveParsedBookToStorageAsync(book.UserId, book.Id,
+                    storageRootPath,
+                        chapters,
+                        token);
+                _logger.LogInformation("[BOOK PROCESSING] SAVED PARSED RESULT. BookId: {BookId}, STATUS:{Status}", bookId, savingResult.Status);
+                book.ParsedFilesPath = savingResult.Path;
+                book.Status = BookStatus.Ready;
+                await _repository.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("[BOOK PROCESSING] FAILED. Message: {Message}", e.Message);
                 return false;
             }
-            var parser = GetParser(book.OriginalFileName);
-            if (parser == null)
-                return false;
-
-            book.Status = BookStatus.ParseProcessing;
-            await _repository.SaveChangesAsync();
-
-            var chapters = await parser.ParseFile(book.StoragePath);
-            var storageRootPath = _config["Storage:ParsedBooksPath"] ?? string.Empty;
-            
-            var savingResult = await _storageService.SaveParsedBookToStorageAsync(book.UserId, book.Id,
-                storageRootPath,
-                    chapters,
-                    token);
-
-            book.ParsedFilesPath = savingResult.Path;
-            book.Status = BookStatus.Ready;
-            await _repository.SaveChangesAsync();
-
-            return true;
         }
 
         private IParser? GetParser(string fileName)
