88f76238a8e7c0a63a8a3cc607f8ea63a36ca941
Oleg <oleg.a.sysa@gmail.com>
2026-08-31 15:21:32 +0300

    added new test logs to event publisher

--- book-reader-backend/BookReader.Infrastructure/Services/Messaging/MassTransitEventPublisher.cs
+++ book-reader-backend/BookReader.Infrastructure/Services/Messaging/MassTransitEventPublisher.cs
@@ -1,20 +1,25 @@
 ﻿using BookReader.Core.Abstract.Events;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 
 namespace BookReader.Infrastructure.Services.Messaging
 {
     public class MassTransitEventPublisher : IEventPublisher
     {
         private readonly ISendEndpointProvider _sendEndpointProvider;
+        private readonly ILogger<MassTransitEventPublisher> _logger;
 
-        public MassTransitEventPublisher(ISendEndpointProvider sendEndpointProvider)
+        public MassTransitEventPublisher(ISendEndpointProvider sendEndpointProvider, ILogger<MassTransitEventPublisher> logger)
         {
             _sendEndpointProvider = sendEndpointProvider;
+            _logger = logger;
         }
 
         public async Task PublishAsync<TEvent>(string queueName, TEvent e, string? correlationId, CancellationToken cancellationToken) 
             where TEvent : IBusinessEvent
         {
+            _logger.LogInformation("Publishing message. CorrelationId: {CorrelationId}", correlationId);
+
             var endpoint = await _sendEndpointProvider.GetSendEndpoint(
                 new Uri($"queue:{queueName}"));
 
